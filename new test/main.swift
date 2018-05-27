d58f190390d99524ee1692641c408e2b71f92e24
Maximal <admin@adm-it.com.ua>
2018-05-27 22:22:55 +0300

    Initial Commit

---
+++ new test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  new test
+//
+//  Created by Павел Бекмансуров on 27.05.2018.
+//  Copyright © 2018 Павел Бекмансуров. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
